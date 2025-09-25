eb9e27dcf0985b6f44a39c8342d8ecec53a5c28a
VIJAY07-KI <bandapallivijay1612@gmail.com>
2025-09-25 21:38:22 +0530

    footer-m

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App: React.FC = () => {
       <OurWork/>
       <Teams/>
       <ContactUs/>
-      <Footer theme={theme} setTheme={setTheme}/>
+      <Footer theme={theme} />
     </div>
   )
 }
